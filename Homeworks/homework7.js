4122a88f33dae5da0c9bc155ad1a33a630350b8a
tokhirboiskhonov <tokhir060@gmail.com>
2023-03-18 19:02:00 +0800

    Lesson#7 and Homework#7 have already done.

---
+++ Homeworks/homework7.js
@@ -0,0 +1,46 @@
+// $$$$$$$$$$$$$$$$$$$$$$$$$$$ Homework#7 $$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+// -- 1 --
+
+// let str = 'webBrain ';
+// console.log(str.repeat(10));
+// console.log(str.replace("web", "IT").repeat(10));
+
+// -- 2 --
+
+// let str = "fozil";
+// let reserveStr = ""
+
+// for(let i = str.length -1; i >= 0; i--)reserveStr += str[i];
+
+// console.log(reserveStr);
+
+// -- 3 --
+
+// let str = '#fozil#web#brain#';
+
+// console.log(str.replace('#', '(').replace('#', '(').replace('#', ')').replace('#', ')'));
+
+// -- 4 --
+
+// let str = 'ABBACC';
+
+// for(let i = 0; i < str.length; i++){
+//     if(str[i] === str[i + 1]){
+//     console.log(str[i]);
+// }
+// }
+
+// -- 5 --
+
+// let str = " Fozil     ";
+
+// console.log(str.trim().endsWith('l'));
+
+// -- 6 --
+
+// let str1 = "Web";
+// let str2 = "Brain";
+
+// console.log(str1.replace('Web', str2));
+// console.log(str2.replace('Brain', str1));
---
+++ Tutorial/lesson7/lesson7.js
@@ -0,0 +1,55 @@
+// ** 7-dars **
+
+// String data type
+
+// syntax => '', "", ``
+
+// let str = "WebBrain";
+
+// console.log(str[0]);
+// console.log(str.length);
+// console.log(str[str.length - 1]); //Bu xolatda oxirgi xarfni olib beradi
+
+// let str1 = 
+// `WebBrain
+// Academy
+// G20`;
+
+// console.log(str1);
+
+// let names = "Anvarjon";
+// let statuss = "Employee";
+
+// let str = `Mening ismim ${names}, He is ${statuss}.`;
+
+// console.log(str);
+
+// let str1 = "WebBrain \n Academy";
+// console.log(str1);
+// let str2 = "WebBrain \t Academy";
+// console.log(str2);
+// let str3 = "WebBrain \r Academy";
+// console.log(str3);
+
+// --- String Method ---
+
+let str = "Webbrain";
+// let str2 = "Academy";
+// console.log(str.toLowerCase()); //webbrain
+// console.log(str.toUpperCase()); //WEBBRAIN
+// console.log(str.startsWith("W")); //true
+// console.log(str.endsWith("s")); //false
+// console.log(str.padStart(10, 'd')); //dddddddWeb
+// console.log(str.padEnd(5, 's')); //Webss
+// console.log(str.replace("Web", "IT")); //"ITbrain"
+// console.log(str.repeat(10));
+// console.log(str.concat(str2)); //WebbrainAcademy
+// console.log(str.includes('e')); //true
+// console.log(str.indexOf('n')); //index boyicha 7 chi
+// console.log(str.lastIndexOf('n')); //oxirgi kegan xarfni olib beradi index ni, ikkita "b" bolsa ham ikkinchisini qberadi.
+// console.log(str.charAt(7)); //"n"
+// console.log(str.substring(2, 7)); // "bbrai"
+// console.log(str.substr(2, 5)); //"bbrai"
+// console.log(str.slice(2, 7)); //"bbrai"
+// console.log(str.split()); //[ 'Webbrain' ]
+// console.log(str.trim().length); //white space ni yoqatadi
