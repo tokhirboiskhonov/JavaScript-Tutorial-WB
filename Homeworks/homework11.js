f6814f68b6296f0c955cc822449c110c019029a3
tokhirboiskhonov <tokhir060@gmail.com>
2023-03-30 17:25:06 +0800

    create function for sum of arr

--- Homeworks/homework11.js
+++ Homeworks/homework11.js
@@ -69,3 +69,21 @@ let users = [
 //     console.log(users);
 // }
 // onDelete(4);
+
+//--9--
+
+// let arr = [1, [3, [5]], [10]];
+// let sum = 0;
+
+// function sumArray(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (Array.isArray(arr[i])) {
+//       sumArray(arr[i]);
+//     } else {
+//       sum += arr[i];
+//     }
+//   }
+// }
+
+// sumArray(arr);
+// console.log(sum); // output: 19
