f47f49cc10bcda1215fc376945455c25631c8b82
tokhirboiskhonov <tokhir060@gmail.com>
2023-04-08 16:35:16 +0800

    updated some info for String methods topic

---
+++ Tutorial/lesson15/script.js
@@ -0,0 +1,126 @@
+// ** 15-dars **
+// ---- Recursion or Recursion function ----
+// -- 1 --
+// function recurse(number) {
+//     console.log(number);
+//     let newNumber = number - 1;
+//     if (newNumber > 0) {
+//         recurse(newNumber);
+//     }
+// }
+
+// recurse(5);
+
+// -- 2 --
+
+// function findSum(n) {
+//     if (n > 0) {
+//         return n + findSum(n - 1);
+//     } else return n;
+// }
+
+// console.log(findSum(5));
+
+// -- 3 --
+
+// let person = {
+//     id: 1,
+//     name: "Odil",
+//     age: 78,
+//     child: {
+//         id: 1,
+//         name: "Ali",
+//         age: 48,
+//         child: {
+//             id: 1,
+//             name: "Umar",
+//             age: 20,
+//         },
+//     },
+// };
+
+// let sum = 0;
+
+// function getAge(obj) {
+//     sum += obj.age;
+//     if (obj.child) {
+//         getAge(obj.child);
+//     }
+// }
+
+// getAge(person);
+// console.log(sum);
+
+// ---- Closure ----
+
+// function parent() {
+//     let num = 10;
+
+//     function child() {
+//         console.log(num);
+//     }
+
+//     child();
+// }
+// parent();
+
+// ---- Higher Order Function ----
+// HOF -->  parametr sifatida function qabul qiladi. (forEach, map, filter, find).
+
+// const arr = [1, 2, 3, 4, 5, 6, 7];
+// arr.forEach((num) => console.log(num + 1));
+
+// ---- Callback Function ---- parametr sifatida keladigan function, bu callback function diyiladi
+
+// let arr = [1, 2, 3, 4, 5];
+// let filter = arr.filter((n) => n > 1);
+// console.log(filter);
+
+// ---- Getter, Setter ----
+// "get" ni vazifasi objectni ichida berilgan funksiyani(methodni) oddiy property qilib chaqirib olish uchun.
+// "set" ni vazifasi esa usha objectni tashqarisida yangi ma'lumot qb berib qoyish. Xar doim parametr oladi.
+
+// const person1 = {
+//     firstName: "Shaxboz",
+//     lastName: "Yaxyoyev",
+
+//     get fullName() {
+//         return `${this.firstName} ${this.lastName}`;
+//     },
+
+//     set fullName(value) {
+//         const values = value.split(" ");
+//         this.firstName = values[0];
+//         this.lastName = values[1];
+//     },
+// };
+
+// person1.fullName = "Tokhir Boiskhonov";
+
+// console.log(person1.fullName);
+
+// ---- Curry function ----
+
+// -- 1 --
+
+// const addition = (a) => {
+//     return (b) => {
+//         return (c) => {
+//             return a + b + c;
+//         };
+//     };
+// };
+
+// console.log(addition(1)(2)(3));
+
+// -- 2 --
+
+// function sendRequest(greet) {
+//     return function (name) {
+//         return function (message) {
+//             return `${greet} ${name} ${message}`;
+//         };
+//     };
+// }
+
+// console.log(sendRequest("Salom")("Tokhir")("Telefonga qaravoring shep!"));
--- Tutorial/lesson7/lesson7.js
+++ Tutorial/lesson7/lesson7.js
@@ -6,11 +6,25 @@
 
 // let str = "WebBrain";
 
+// let str1 = "webbrain";
+// let str2 = new String("webbrain");
+// let str2 = "Web \tBrain";
+// console.log(str1 === "webbrain"); //true
+// console.log(str2 === "webbrain"); //false, because constructor function always return object data type
+// console.log(str1 === str2); //false
+// console.log(str1.localeCompare(str2)); //output: 0
+//When we need to compare two stirngs, so we have to use localeCompare, and this comparision can be return answer like this: -1 --> bu ummuman tog'ri kelmidi, 0 --> esa to'liq tog'ri keladi, 1--> qisamn tog'ri keladi
+
+// let str1 = "webbrain";
+// console.log(str1[0]); //output: w
+// console.log(str1.charAt(0)); //output: w // minuslik value ni qabul qilmaydi
+// console.log(str1.at(-1)); //output: n //minuslik value ni qabul qiladi
+
 // console.log(str[0]);
 // console.log(str.length);
 // console.log(str[str.length - 1]); //Bu xolatda oxirgi xarfni olib beradi
 
-// let str1 = 
+// let str1 =
 // `WebBrain
 // Academy
 // G20`;
@@ -33,19 +47,21 @@
 
 // --- String Method ---
 
-let str = "Webbrain";
+// let str = "Webbrain Academy";
 // let str2 = "Academy";
 // console.log(str.toLowerCase()); //webbrain
 // console.log(str.toUpperCase()); //WEBBRAIN
 // console.log(str.startsWith("W")); //true
 // console.log(str.endsWith("s")); //false
 // console.log(str.padStart(10, 'd')); //dddddddWeb
-// console.log(str.padEnd(5, 's')); //Webss
+// console.log(str.padEnd(5, "s")); //Webss
 // console.log(str.replace("Web", "IT")); //"ITbrain"
 // console.log(str.repeat(10));
 // console.log(str.concat(str2)); //WebbrainAcademy
+// console.log(str.concat(str, "Hey")); //Webbrain AcademyWebbrain AcademyHey
 // console.log(str.includes('e')); //true
 // console.log(str.indexOf('n')); //index boyicha 7 chi
+// console.log(str.indexOf("b", 5)); //'b' shu indexni 5 chi indexdan boshlab qidir dgan manoda keladi
 // console.log(str.lastIndexOf('n')); //oxirgi kegan xarfni olib beradi index ni, ikkita "b" bolsa ham ikkinchisini qberadi.
 // console.log(str.charAt(7)); //"n"
 // console.log(str.substring(2, 7)); // "bbrai"
@@ -53,3 +69,24 @@ let str = "Webbrain";
 // console.log(str.slice(2, 7)); //"bbrai"
 // console.log(str.split()); //[ 'Webbrain' ]
 // console.log(str.trim().length); //white space ni yoqatadi
+// console.log(`${str}`.toLocaleLowerCase());
+// console.log(str.trimStart());
+// console.log(str.trimEnd());
+// let str2 = "webbrain academy"
+// console.log(str2.split(' '));
+// console.log(str2.slice(0, 4)); //webb //0 chi indexdan 4 chi indexgacha olarkan, bu dgani 4-index include bo'midi.
+// console.log(str2.substr(2, 2));
+// console.log(new String("webbrain").valueOf()); //output: webbrain
+// console.log(new String('webbrain')); //output: [String: 'webbrain']
+
+let str = "web brain acad web emy";
+
+console.log(str.replace(/web/i, "WEB")); //ignore qiladi birinchi kelgan oxshash ma'lumotni ignore qilib yangisini qoyib beradi, output: WEB brain acad emy
+
+let str1 = "web brain acad web emy";
+console.log(str1.replace(/web/i, "WEB")); //output: WEB brain acad web emy (birinchi kelgan tori ma'lumotni o'zgartirdi qoganiniga yetib bormadi).
+
+let str2 = "web brain acad web emy";
+// console.log(str2.replace(/web/ig, "WEB")); //output: WEB brain acad WEB emy global ignore qiladi bu dagni bitta stringni ichida nechi marotaba kelgan bolsa ham ignore qialdi
+// console.log(str2.replaceAll('web', 'WEB')); //output: WEB brain acad WEB emy
+console.log(str2.split(" ", 3)); //output: [ 'web', 'brain', 'acad' ]
