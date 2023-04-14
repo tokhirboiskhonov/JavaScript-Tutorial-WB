80600c1c44cc6a990f5ba3de3880d56f4c075e29
tokhirboiskhonov <tokhir060@gmail.com>
2023-04-14 08:41:55 +0800

    Module JS - export, import

---
+++ Tutorial/lesson17/script1.mjs
@@ -0,0 +1,27 @@
+// ** 17-dars **
+
+//Module in JS --> export and import
+
+// 1-version
+
+// function greeting() {
+//     console.log("Hello G20 group");
+// }
+
+// const num = 100;
+
+// export { greeting, num };
+
+// 2-version
+
+// function greeting() {
+//     console.log("Hello G20 group");
+// }
+
+// export default greeting;
+
+// 3-version
+
+// export function greeting(){
+//     console.log("Hello G20 group");
+// }
---
+++ Tutorial/lesson17/script2.mjs
@@ -0,0 +1,17 @@
+// 1-version
+
+// import { greeting, num } from "./script1.mjs";
+
+// greeting();
+// console.log(num);
+
+// 2-version
+
+// import Salom from "./script1.mjs";
+// Salom()
+
+// 3-version
+
+// import { greeting } from "./script1.mjs";
+
+// greeting();
